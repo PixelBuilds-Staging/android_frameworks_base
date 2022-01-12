9cad18cac33bc1f853175225bcaf94daa12056e7
SagarMakhar <sagarmakhar@gmail.com>
2022-01-12 18:32:54 +0000

    [SQUASH] Black theme [1/2]
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: Ia03a56b7472c19a68453c616dfb420793ce34e1b
    
    [ExactExampl]: Squashed with following changes:
    
    commit 1590f8b66e1f6def664eb8ad743b0ff0c4828f4e
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Sat Apr 16 10:38:36 2022 +0530
    
        Fixes and improvements for Black Theme [1/2]
    
        * Add to SettingsObserver for observing toggle behavior.
        * Apply only with dark theme - looks awful on light mode.
        * Add configuration listener to reapply theme when dark mode changes.
        * Remove repeated monet check
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
        Signed-off-by: Mayur <ultramayur123@gmail.com>
        Signed-off-by: spezi77 <spezi7713@gmx.net>
    
    commit 223a4982fde279744240b127baa5ab0a57ad9d62
    Author: spezi77 <spezi7713@gmx.net>
    Date:   Thu Jul 6 19:14:30 2023 +0200
    
        SystemUI: ThemeOverlayController: Change black theme observer into a tuneable.
    
        Change-Id: I048a8a88c56779a0b3bc9482a08b6a1aeb2a7ba1
        Signed-off-by: spezi77 <spezi7713@gmx.net>
    
    commit 42a985aa9ffccb9c054e2bad386b273cf7f11198
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Mon Nov 21 18:30:02 2022 +0530
    
        SystemUI: Reload theme only if black theme is enabled
    
        Currently it force reloads theme on UI mode change always
        for black theme to set correctly, But this has the side
        effect of janky transition between dark<->light mode and
        other glitches in QS. Perform this only if we have black
        theme actually enabled.
    
        Change-Id: I372bdb51dacc54305d47cd42a5b165ac35609bd8
        Signed-off-by: DennySPb <dennyspb@gmail.com>
        Signed-off-by: spezi77 <spezi7713@gmx.net>
    
    Change-Id: I05fbdfe213e35756d1cf895c17a7defccc268e56

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -78,9 +78,12 @@ import com.android.systemui.monet.ColorScheme;
 import com.android.systemui.monet.Style;
 import com.android.systemui.monet.TonalPalette;
 import com.android.systemui.settings.UserTracker;
+import com.android.systemui.statusbar.policy.ConfigurationController;
+import com.android.systemui.statusbar.policy.ConfigurationController.ConfigurationListener;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController.DeviceProvisionedListener;
 import com.android.systemui.util.kotlin.JavaAdapter;
+import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.settings.SecureSettings;
 
 import com.google.ux.material.libmonet.dynamiccolor.MaterialDynamicColors;
@@ -120,9 +123,11 @@ import javax.inject.Inject;
  * associated work profiles
  */
 @SysUISingleton
-public class ThemeOverlayController implements CoreStartable, Dumpable {
+public class ThemeOverlayController implements CoreStartable, Dumpable, TunerService.Tunable {
     protected static final String TAG = "ThemeOverlayController";
     private static final boolean DEBUG = true;
+
+    private static final String SYSTEM_BLACK_THEME = Settings.Secure.SYSTEM_BLACK_THEME;
 
     private final ThemeOverlayApplier mThemeManager;
     private final UserManager mUserManager;
@@ -169,11 +174,14 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
     private final UiModeManager mUiModeManager;
     private DynamicScheme mDynamicSchemeDark;
     private DynamicScheme mDynamicSchemeLight;
+    private final TunerService mTunerService;
 
     // Defers changing themes until Setup Wizard is done.
     private boolean mDeferredThemeEvaluation;
     // Determines if we should ignore THEME_CUSTOMIZATION_OVERLAY_PACKAGES setting changes.
     private boolean mSkipSettingChange;
+
+    private final ConfigurationController mConfigurationController;
 
     private final DeviceProvisionedListener mDeviceProvisionedListener =
             new DeviceProvisionedListener() {
@@ -414,7 +422,9 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
             WakefulnessLifecycle wakefulnessLifecycle,
             JavaAdapter javaAdapter,
             KeyguardTransitionInteractor keyguardTransitionInteractor,
-            UiModeManager uiModeManager) {
+            UiModeManager uiModeManager,
+            ConfigurationController configurationController,
+            TunerService tunerService) {
         mContext = context;
         mIsMonetEnabled = featureFlags.isEnabled(Flags.MONET);
         mIsFidelityEnabled = featureFlags.isEnabled(Flags.COLOR_FIDELITY);
@@ -433,6 +443,8 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
         mJavaAdapter = javaAdapter;
         mKeyguardTransitionInteractor = keyguardTransitionInteractor;
         mUiModeManager = uiModeManager;
+        mConfigurationController = configurationController;
+        mTunerService = tunerService;
         dumpManager.registerDumpable(TAG, this);
     }
 
@@ -476,10 +488,6 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
             reevaluateSystemTheme(true /* forceReload */);
         });
 
-        if (!mIsMonetEnabled) {
-            return;
-        }
-
         mUserTracker.addCallback(mUserTrackerCallback, mMainExecutor);
 
         mDeviceProvisionedController.addCallback(mDeviceProvisionedListener);
@@ -488,6 +496,8 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
         if (!mIsMonetEnabled) {
             return;
         }
+
+        mTunerService.addTunable(this, SYSTEM_BLACK_THEME);
 
         // Upon boot, make sure we have the most up to date colors
         Runnable updateColors = () -> {
@@ -542,6 +552,18 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                 }
             });
         }
+        mConfigurationController.addCallback(mConfigurationListener);
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        switch (key) {
+            case SYSTEM_BLACK_THEME:
+                reevaluateSystemTheme(true /* forceReload */);
+                break;
+            default:
+                break;
+         }
     }
 
     private void reevaluateSystemTheme(boolean forceReload) {
@@ -816,6 +838,13 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
                     .map(key -> key + " -> " + categoryToPackage.get(key)).collect(
                             Collectors.joining(", ")));
         }
+
+        boolean nightMode = (mContext.getResources().getConfiguration().uiMode
+                & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
+        boolean isBlackTheme = isBlackThemeEnabled() && nightMode;
+
+        mThemeManager.setIsBlackTheme(isBlackTheme);
+
         if (mNeedsOverlayCreation) {
             mNeedsOverlayCreation = false;
             mThemeManager.applyCurrentUserOverlays(categoryToPackage, new FabricatedOverlay[]{
@@ -825,6 +854,8 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
             mThemeManager.applyCurrentUserOverlays(categoryToPackage, null, currentUser,
                     managedProfiles);
         }
+
+        mThemeManager.applyBlackTheme(isBlackTheme);
     }
 
     private Style fetchThemeStyleFromSetting() {
@@ -854,6 +885,19 @@ public class ThemeOverlayController implements CoreStartable, Dumpable {
         }
         return style;
     }
+
+    private boolean isBlackThemeEnabled() {
+        return mSecureSettings.getInt(Settings.Secure.SYSTEM_BLACK_THEME, 0) == 1;
+    }
+
+    private final ConfigurationListener mConfigurationListener = new ConfigurationListener() {
+        @Override
+        public void onUiModeChanged() {
+            if (isBlackThemeEnabled()) {
+                reevaluateSystemTheme(true /* forceReload */);
+            }
+        }
+    };
 
     @Override
     public void dump(@NonNull PrintWriter pw, @NonNull String[] args) {
