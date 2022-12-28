c8c18ad44d82374cc8e42fea88bbcea83d6ec7d4
jhenrique09 <jhsv09@gmail.com>
2022-12-27 23:14:00 -0800

    SystemUI: Introduce Ambient Music detection support on Lock screen
    * Show track info of detected ambient music on lock screen
    * Ported from StatixOS by @ Albinoman887

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIBinder.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIBinder.java
@@ -16,11 +16,17 @@
 
 package com.android.systemui.dagger;
 
+import com.android.systemui.CoreStartable;
 import com.android.systemui.keyguard.dagger.KeyguardModule;
 import com.android.systemui.recents.RecentsModule;
 import com.android.systemui.statusbar.dagger.CentralSurfacesModule;
 
+import com.pb.android.systemui.PBServices;
+
+import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.ClassKey;
+import dagger.multibindings.IntoMap;
 
 /**
  * SystemUI objects that are injectable should go here.
@@ -31,4 +37,12 @@ import dagger.Module;
         KeyguardModule.class,
 })
 public abstract class SystemUIBinder {
+
+    /**
+     * Inject into PBServices.
+     */
+    @Binds
+    @IntoMap
+    @ClassKey(PBServices.class)
+    public abstract CoreStartable bindPBServices(PBServices sysui);
 }
