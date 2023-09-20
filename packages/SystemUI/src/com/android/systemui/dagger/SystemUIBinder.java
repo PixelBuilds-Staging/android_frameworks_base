all-backupss/_frameworks_base

SystemUI: Introduce Ambient Music detection support on Lock screen
* Show track info of detected ambient music on lock screen
* Ported from StatixOS by @Albinoman887

packages/SystemUI/src/com/android/systemui/dagger/SystemUIBinder.java
@@ -16,11 +16,17 @@
 
 package com.android.systemui.dagger;
 
+import com.android.systemui.CoreStartable;
 import com.android.systemui.keyguard.dagger.KeyguardModule;
 import com.android.systemui.recents.RecentsModule;
 import com.android.systemui.statusbar.dagger.CentralSurfacesModule;
 
+import com.superior.android.systemui.SuperiorServices;
+
+import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.ClassKey;
+import dagger.multibindings.IntoMap;
 
 /**
  * SystemUI objects that are injectable should go here.
@@ -31,4 +37,12 @@ import dagger.Module;
         KeyguardModule.class,
 })
 public abstract class SystemUIBinder {
+
+    /**
+     * Inject into SuperiorServices.
+     */
+    @Binds
+    @IntoMap
+    @ClassKey(SuperiorServices.class)
+    public abstract CoreStartable bindSuperiorServices(SuperiorServices sysui);
 }

packages/SystemUI/src/com/superior/android/systemui/ambient/AmbientIndicationService.java
@@ -1,0 +1,112 @@
+/*
+ * Copyright (C) 2022 The PixelExperience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.superior.android.systemui.ambient;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.SystemClock;
+import android.os.UserHandle;
+import android.util.Log;
+
+import com.android.keyguard.KeyguardUpdateMonitor;
+import com.android.keyguard.KeyguardUpdateMonitorCallback;
+import com.android.systemui.Dependency;
+
+public class AmbientIndicationService extends BroadcastReceiver {
+    private final AlarmManager mAlarmManager;
+    private final AmbientIndicationContainer mAmbientIndicationContainer;
+    private final Context mContext;
+    private final KeyguardUpdateMonitorCallback mCallback = new KeyguardUpdateMonitorCallback() {
+        @Override
+        public void onUserSwitchComplete(int i) {
+            onUserSwitched();
+        }
+    };
+    private final AlarmManager.OnAlarmListener mHideIndicationListener;
+
+    private static final String HIDE_AMBIENT_ACTION = "com.google.android.ambientindication.action.AMBIENT_INDICATION_HIDE";
+    private static final String SHOW_AMBIENT_ACTION = "com.google.android.ambientindication.action.AMBIENT_INDICATION_SHOW";
+
+    public AmbientIndicationService(Context context, AmbientIndicationContainer ambientIndicationContainer, AlarmManager alarmManager) {
+        mContext = context;
+        mAmbientIndicationContainer = ambientIndicationContainer;
+        mAlarmManager = alarmManager;
+        mHideIndicationListener = () -> mAmbientIndicationContainer.hideAmbientMusic();
+        start();
+    }
+
+    void start() {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(SHOW_AMBIENT_ACTION);
+        intentFilter.addAction(HIDE_AMBIENT_ACTION);
+        mContext.registerReceiverAsUser(this, UserHandle.ALL, intentFilter, "com.google.android.ambientindication.permission.AMBIENT_INDICATION", null);
+        ((KeyguardUpdateMonitor) Dependency.get(KeyguardUpdateMonitor.class)).registerCallback(mCallback);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (!isForCurrentUser()) {
+            Log.i("AmbientIndication", "Suppressing ambient, not for this user.");
+        } else if (verifyAmbientApiVersion(intent)) {
+            String action = intent.getAction();
+            if (action.equals(HIDE_AMBIENT_ACTION)) {
+                mAlarmManager.cancel(mHideIndicationListener);
+                mAmbientIndicationContainer.hideAmbientMusic();
+                Log.i("AmbientIndication", "Hiding ambient indication.");
+            } else if (action.equals(SHOW_AMBIENT_ACTION)) {
+                long min = Math.min(Math.max(intent.getLongExtra("com.google.android.ambientindication.extra.TTL_MILLIS", 180000L), 0L), 180000L);
+                boolean booleanExtra = intent.getBooleanExtra("com.google.android.ambientindication.extra.SKIP_UNLOCK", false);
+                int intExtra = intent.getIntExtra("com.google.android.ambientindication.extra.ICON_OVERRIDE", 0);
+                String stringExtra = intent.getStringExtra("com.google.android.ambientindication.extra.ICON_DESCRIPTION");
+                mAmbientIndicationContainer.setAmbientMusic(
+                        intent.getCharSequenceExtra("com.google.android.ambientindication.extra.TEXT").toString(),
+                        (PendingIntent) intent.getParcelableExtra("com.google.android.ambientindication.extra.OPEN_INTENT"),
+                        (PendingIntent) intent.getParcelableExtra("com.google.android.ambientindication.extra.FAVORITING_INTENT"),
+                        booleanExtra,
+                        intExtra,
+                        stringExtra);
+                mAlarmManager.setExact(2, SystemClock.elapsedRealtime() + min, "AmbientIndication", mHideIndicationListener, null);
+                Log.i("AmbientIndication", "Showing ambient indication.");
+            }
+        }
+    }
+
+    private boolean verifyAmbientApiVersion(Intent intent) {
+        int intExtra = intent.getIntExtra("com.google.android.ambientindication.extra.VERSION", 0);
+        if (intExtra != 1) {
+            Log.e("AmbientIndication", "AmbientIndicationApi.EXTRA_VERSION is 1, but received an intent with version " + intExtra + ", dropping intent.");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isForCurrentUser() {
+        return getSendingUserId() == getCurrentUser() || getSendingUserId() == -1;
+    }
+
+    private int getCurrentUser() {
+        return KeyguardUpdateMonitor.getCurrentUser();
+    }
+
+    private void onUserSwitched() {
+        mAmbientIndicationContainer.hideAmbientMusic();
+    }
+}

PixelBuilds-Staging/android_frameworks_base

* Ported from StatixOS by @ Albinoman887

packages/SystemUI/src/com/android/systemui/dagger/SystemUIBinder.java
@@ -16,11 +16,17 @@
 
 package com.android.systemui.dagger;
 
+import com.android.systemui.CoreStartable;
 import com.android.systemui.keyguard.dagger.KeyguardModule;
 import com.android.systemui.recents.RecentsModule;
 import com.android.systemui.statusbar.dagger.CentralSurfacesModule;
 
+import com.pb.android.systemui.PBServices;
+
+import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.ClassKey;
+import dagger.multibindings.IntoMap;
 
 /**
  * SystemUI objects that are injectable should go here.
@@ -31,4 +37,12 @@ import dagger.Module;
         KeyguardModule.class,
 })
 public abstract class SystemUIBinder {
+
+    /**
+     * Inject into PBServices.
+     */
+    @Binds
+    @IntoMap
+    @ClassKey(PBServices.class)
+    public abstract CoreStartable bindPBServices(PBServices sysui);
 }

packages/SystemUI/src/com/pb/android/systemui/ambient/AmbientIndicationService.java
@@ -1,0 +1,112 @@
+/*
+ * Copyright (C) 2022 The PixelExperience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pb.android.systemui.ambient;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.SystemClock;
+import android.os.UserHandle;
+import android.util.Log;
+
+import com.android.keyguard.KeyguardUpdateMonitor;
+import com.android.keyguard.KeyguardUpdateMonitorCallback;
+import com.android.systemui.Dependency;
+
+public class AmbientIndicationService extends BroadcastReceiver {
+    private final AlarmManager mAlarmManager;
+    private final AmbientIndicationContainer mAmbientIndicationContainer;
+    private final Context mContext;
+    private final KeyguardUpdateMonitorCallback mCallback = new KeyguardUpdateMonitorCallback() {
+        @Override
+        public void onUserSwitchComplete(int i) {
+            onUserSwitched();
+        }
+    };
+    private final AlarmManager.OnAlarmListener mHideIndicationListener;
+
+    private static final String HIDE_AMBIENT_ACTION = "com.google.android.ambientindication.action.AMBIENT_INDICATION_HIDE";
+    private static final String SHOW_AMBIENT_ACTION = "com.google.android.ambientindication.action.AMBIENT_INDICATION_SHOW";
+
+    public AmbientIndicationService(Context context, AmbientIndicationContainer ambientIndicationContainer, AlarmManager alarmManager) {
+        mContext = context;
+        mAmbientIndicationContainer = ambientIndicationContainer;
+        mAlarmManager = alarmManager;
+        mHideIndicationListener = () -> mAmbientIndicationContainer.hideAmbientMusic();
+        start();
+    }
+
+    void start() {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(SHOW_AMBIENT_ACTION);
+        intentFilter.addAction(HIDE_AMBIENT_ACTION);
+        mContext.registerReceiverAsUser(this, UserHandle.ALL, intentFilter, "com.google.android.ambientindication.permission.AMBIENT_INDICATION", null);
+        ((KeyguardUpdateMonitor) Dependency.get(KeyguardUpdateMonitor.class)).registerCallback(mCallback);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (!isForCurrentUser()) {
+            Log.i("AmbientIndication", "Suppressing ambient, not for this user.");
+        } else if (verifyAmbientApiVersion(intent)) {
+            String action = intent.getAction();
+            if (action.equals(HIDE_AMBIENT_ACTION)) {
+                mAlarmManager.cancel(mHideIndicationListener);
+                mAmbientIndicationContainer.hideAmbientMusic();
+                Log.i("AmbientIndication", "Hiding ambient indication.");
+            } else if (action.equals(SHOW_AMBIENT_ACTION)) {
+                long min = Math.min(Math.max(intent.getLongExtra("com.google.android.ambientindication.extra.TTL_MILLIS", 180000L), 0L), 180000L);
+                boolean booleanExtra = intent.getBooleanExtra("com.google.android.ambientindication.extra.SKIP_UNLOCK", false);
+                int intExtra = intent.getIntExtra("com.google.android.ambientindication.extra.ICON_OVERRIDE", 0);
+                String stringExtra = intent.getStringExtra("com.google.android.ambientindication.extra.ICON_DESCRIPTION");
+                mAmbientIndicationContainer.setAmbientMusic(
+                        intent.getCharSequenceExtra("com.google.android.ambientindication.extra.TEXT").toString(),
+                        (PendingIntent) intent.getParcelableExtra("com.google.android.ambientindication.extra.OPEN_INTENT"),
+                        (PendingIntent) intent.getParcelableExtra("com.google.android.ambientindication.extra.FAVORITING_INTENT"),
+                        booleanExtra,
+                        intExtra,
+                        stringExtra);
+                mAlarmManager.setExact(2, SystemClock.elapsedRealtime() + min, "AmbientIndication", mHideIndicationListener, null);
+                Log.i("AmbientIndication", "Showing ambient indication.");
+            }
+        }
+    }
+
+    private boolean verifyAmbientApiVersion(Intent intent) {
+        int intExtra = intent.getIntExtra("com.google.android.ambientindication.extra.VERSION", 0);
+        if (intExtra != 1) {
+            Log.e("AmbientIndication", "AmbientIndicationApi.EXTRA_VERSION is 1, but received an intent with version " + intExtra + ", dropping intent.");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isForCurrentUser() {
+        return getSendingUserId() == getCurrentUser() || getSendingUserId() == -1;
+    }
+
+    private int getCurrentUser() {
+        return KeyguardUpdateMonitor.getCurrentUser();
+    }
+
+    private void onUserSwitched() {
+        mAmbientIndicationContainer.hideAmbientMusic();
+    }
+}